91ba0b4c85e562e7461edd2117408394c71d5563
ravielze <ravielz20@gmail.com>
2020-11-21 03:25:51 +0700

    Item

---
+++ entity/item.pl
@@ -0,0 +1,33 @@
+/* Fakta mengenai equipment (menambah berapa attack, defense, dll.) dan potion */
+
+/* Equipments */
+/* Format: ID, Name, att, def, HP */
+equipment(0, "He em", 3, 0, 0). 
+equipment(1, "Manna", 5, 0, 0).
+equipment(2, "Silicon", 7, 0, 0).
+equipment(3, "Nomiap", 0, 5, 0).
+equipment(4, "Saiacan Icant", 0, 7, 3).
+equipment(5, "Teesllat", 2, 4, 3).
+equipment(6, "Cabooru", 3, 7, 6).
+
+/* Facts for weapons */
+weapon(0).
+weapon(1).
+weapon(2).
+
+/* Facts for armor */
+armor(3).
+armor(4).
+
+/* Facts for accessories */
+accessories(5).
+accessories(6).
+
+/* Potions */
+/* Format: ID, Name, att, def, HP, price */
+potion(0, "Yuk", 0, 0, 5, 5).
+potion(1, "Bisa Yucc", 0, 2, 4, 7).
+potion(2, "IF2121", 5, 5, 0, 14).
+potion(3, "Assembly", 7, 2, 3, 18).
+potion(4, "Emergency Food", 2, 10, 10, 25).
+potion(5, "Childe", 10, 10, 10, 30).
