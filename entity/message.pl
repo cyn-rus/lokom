0d296040c6383e8b5a2b5ba9f8a1ffaa63a5433c
ravielze <ravielz20@gmail.com>
2020-11-21 18:12:48 +0700

    Fix message

--- entity/message.pl
+++ entity/message.pl
@@ -1,8 +1,9 @@
 msg_game_not_started("Game has not started, use \'start.\' to play the game.").
 msg_crush_wall("You crush the wall.").
-msg_move("W", Y) :- Y is "You move north.", !.
-msg_move("S", Y) :- Y is "You move south.", !.
-msg_move("A", Y) :- Y is "You move west.", !.
-msg_move("D", Y) :- Y is "You move east.", !.
+msg_move("W", "You move north.").
+msg_move("S", "You move south.").
+msg_move("A", "You move west.").
+msg_move("D", "You move east.").
 msg_in_battle("You can't do that. You are in battle.").
 msg_store_not_near("You can't open store here.").
+msg_store_gold_not_enough("Gold not enough. Go kill some enemies or clear some quests.").
